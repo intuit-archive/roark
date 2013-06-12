879622ebe9045209741ffa2475a7ab8882ef2340
Brett Weaver <brett@weav.net>
2013-06-11 22:30:12 -0700

    fixing cli

--- lib/roark/cli/create.rb
+++ lib/roark/cli/create.rb
@@ -6,14 +6,58 @@ module Roark
         @options = {}
       end
 
+      def create
+        option_parser.parse!
+
+        template   = File.read @options[:template]
+
+        image = Roark::Image.new :aws_access_key => @options[:aws_access_key],
+                                 :aws_secret_key => @options[:aws_secret_key],
+                                 :name           => @options[:name],
+                                 :region         => @options[:region]
+
+        image.create :template   => template,
+                     :parameters => parse_parameters(@options[:parameters])
+      end
+
       def option_parser
         OptionParser.new do |opts|
           opts.banner = "Usage: roark create [options]"
 
-          opts.on("-n", "--name [NAME]", "Name of Image") do |n|
-            @options[:name] = n
+          opts.on("-n", "--name [NAME]", "Name of image") do |o|
+            @options[:name] = o
+          end
+
+          opts.on("-p", "--parameters [PARAMETERS]", "CSV of parameters and values separated by '=' to pass to Cloud Fomration") do |o|
+            @options[:parameters] = o
+          end
+
+          opts.on("-r", "--region [REGION]", "Region to build image") do |o|
+            @options[:region] = o
+          end
+
+          opts.on("-t", "--template [TEMPLATE]", "Path to Cloud Formation template") do |o|
+            @options[:template] = o
+          end
+
+          opts.on("--aws-access-key [KEY]", "AWS Access Key") do |o|
+            @options[:aws_access_key] = o
+          end
+
+          opts.on("--aws-secret-key [KEY]", "AWS Secret Key") do |o|
+            @options[:aws_secret_key] = o
           end
         end
+      end
+
+      def parse_parameters(parameters)
+        p = {}
+        parameters.split(',').each do |attribs|
+          key   = attribs.split('=').first.gsub(/\s+/, "")
+          value = attribs.gsub(/^.+?=/, '')
+          p[key] = value
+        end
+        p
       end
 
       def command_summary
