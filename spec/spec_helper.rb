9989e9b1b09e60934cfa4572fd054f94509cfa49
Brett Weaver <brett@weav.net>
2013-06-13 19:51:02 -0700

    removing unused code

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,11 +2,3 @@ require 'rubygems'
 require 'bundler/setup'
 
 require 'roark'
-
-Dir[File.expand_path(File.join(File.dirname(__FILE__),'helpers', '*.rb'))].each do |f|
-  require f
-end
-
-RSpec.configure do |config|
-  config.include Fixtures
-end
